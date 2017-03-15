bd9820b20356425eae8c7146a112b6e9cef63019
Atul Kumar <atulbk@thoughtworks.com>
2017-03-16 00:49:07 +0530

    Created a server-client app

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from "express";
+const app = express();
+
+app.set('port', 3001);
+
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+
+app.listen(app.get('port'), () => {
+  console.log(`Find the server at: http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
+
+});
